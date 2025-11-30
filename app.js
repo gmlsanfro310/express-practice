2502d90419b0aa18220b9c9205f0aa2b731bba59
gmlsanfro <gmlsanfro@ajou.ac.kr>
2025-11-30 22:10:46 +0900

    POST

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
-const express = require('express')
-const app = express()
+var express = require('express');
+const logger = require('morgan');
+var app = express()
 const port = 3000
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(logger('dev'));
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
-  })
-  app.get('/user/:id', (req, res) => {
+})
+
+app.get('/user/:id', (req, res) => {
     res.send(`User id is ${req.params.id}`);
-  })
- 
+})
+
 app.get('/user', (req, res) => {
     res.send(`User id is ${req.query.id}`);
-  })
-  app.listen(port, () => {
+})
+
+app.post('/user', (reaq, res) => {
+    console.log(req.body.name);
+    res.send(req.body);
+})
+
+
+app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
-  })
+})
