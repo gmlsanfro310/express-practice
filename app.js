3f22fa0fdbb66561ba0cf6e204ac418398f5a051
gmlsanfro <gmlsanfro@ajou.ac.kr>
2025-11-30 23:00:17 +0900

    firebase done

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 const logger = require('morgan');
 const axios = require('axios');
 const list = require('./data');
+const firebase = require('./firebase');
 
 var app = express()
 const port = 3000
@@ -9,10 +10,10 @@ const port = 3000
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(logger('dev'));
-
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.sendFile('index.html')
 })
 
 app.get('/user/:id', (req, res) => {
@@ -28,9 +29,40 @@ app.post('/user', (reaq, res) => {
     res.send(req.body);
 })
 
-app.get('/music_list', (req,res) => {
+app.get('/music_list', (req, res) => {
     res.json(list);
 })
+
+app.get('/likes', async (req, res) => {
+    var db = firebase.firestore();
+    const snapshot = await db.collection('likes').get().catch(e => console.log(e));
+    var results = [];
+    if (snapshot.empty) {
+        console.log("No result");
+        res.json([]);
+        return;
+    } else {
+        snapshot.forEach(doc => {
+            results.push(doc.data())
+            //console.log(doc.id, '=>', doc.data());
+        })
+        res.json(results);
+    }
+})
+
+app.post('/likes', async (req, res) => {
+    let item = req.body;
+    let db = firebase.firestore();
+    let r = await db.collection('likes').doc(item.collectionId.toString()).set(item);
+    
+    res.json({msg : 'OK'});
+  })
+  
+  app.delete('/likes/:id', async (req, res) => {
+    let db = firebase.firestore();
+    let r = await db.collection('likes').doc(req.params.id).delete();
+    res.json({msg : 'OK'});
+  })
 
 app.get('/musicSearch/:term', async (req, res) => {
     const params = {
