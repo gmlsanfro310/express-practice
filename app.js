80a10ecca9be4c780decac7c13839187fa268c20
gmlsanfro <gmlsanfro@ajou.ac.kr>
2025-11-30 21:57:49 +0900

    req

--- app.js
+++ app.js
@@ -5,7 +5,13 @@ const port = 3000
 app.get('/', (req, res) => {
     res.send('Hello World!')
   })
-  
+  app.get('/user/:id', (req, res) => {
+    res.send(`User id is ${req.params.id}`);
+  })
+ 
+app.get('/user', (req, res) => {
+    res.send(`User id is ${req.query.id}`);
+  })
   app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
   })
