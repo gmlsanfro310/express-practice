2f705868883303609b24458ff13e83e9cc1590a6
gmlsanfro <gmlsanfro@ajou.ac.kr>
2025-12-09 16:25:14 +0900

    fix3

--- firebase.js
+++ firebase.js
@@ -1,7 +1,7 @@
 var firebase = require("firebase-admin");
 
 var serviceAccount = require("./firebaseKey.json");
-
+//var serviceAccount = JSON.parse(process.env.FIREBASE_KEY);
 firebase.initializeApp({
   credential: firebase.credential.cert(serviceAccount)
 });
